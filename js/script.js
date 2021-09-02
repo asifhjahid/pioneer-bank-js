33dce159bca578cf5d3f4731e09a526f80ee119d
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-03 01:10:36 +0600

    deposit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
+//login button event handler
+
 let button = document.querySelector('#submitBtn');
 button.addEventListener('click',()=>{
      let loginPage = document.querySelector('#login-page');
      loginPage.style.display='none';
      let transaction = document.querySelector('#transaction');
      transaction.style.display='block'
+})
+
+//deposit button event handler
+
+let depositBtn =  document.querySelector('#deposit-btn');
+depositBtn.addEventListener('click',()=>{
+    let depositAmount = document.querySelector('#deposit-amount').value;
+    let depositMoney = parseFloat(depositAmount);
+    
+
+    let currentDeposit = document.querySelector('#current-deposit').innerText;
+    let currentDepositAmount = parseFloat(currentDeposit);
+    let totalDeposit = currentDepositAmount + depositMoney;
+    document.querySelector('#current-deposit').innerText = totalDeposit;
+    document.querySelector('#deposit-amount').value = "";
 })
