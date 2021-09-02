66511fa26dcbe9cd3b43a71e83393e0624bcb4d2
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-03 03:17:10 +0600

    withdraw

--- js/script.js
+++ js/script.js
@@ -12,13 +12,47 @@ button.addEventListener('click',()=>{
 
 let depositBtn =  document.querySelector('#deposit-btn');
 depositBtn.addEventListener('click',()=>{
-    let depositAmount = document.querySelector('#deposit-amount').value;
-    let depositMoney = parseFloat(depositAmount);
+    let depositMoney = inputValue('#deposit-amount');
     
-
-    let currentDeposit = document.querySelector('#current-deposit').innerText;
-    let currentDepositAmount = parseFloat(currentDeposit);
-    let totalDeposit = currentDepositAmount + depositMoney;
-    document.querySelector('#current-deposit').innerText = totalDeposit;
+    
+    updatedAmount('#current-deposit',depositMoney)
+    // let currentDeposit = document.querySelector('#current-deposit').innerText;
+    // let currentDepositAmount = parseFloat(currentDeposit);
+    // let totalDeposit = currentDepositAmount + depositMoney;
+    // document.querySelector('#current-deposit').innerText = totalDeposit;
     document.querySelector('#deposit-amount').value = "";
+    
+    updatedAmount("#current-balance",depositMoney)
+
+    // let currentBalance = document.querySelector('#current-balance').innerText;
+    // let currentBalanceAmount = parseFloat(currentBalance);
+    // let totalBalance = currentBalanceAmount + depositMoney;
+    
+    // document.querySelector('#current-balance').innerText = totalBalance;
 })
+ 
+
+//withdraw button event handler
+
+let withdrawBtn = document.querySelector('#withdraw-btn');
+withdrawBtn.addEventListener('click',()=>{
+    let withdrawAmount = inputValue('#withdraw-amount');
+    updatedAmount('#current-withdraw',withdrawAmount);
+    updatedAmount("#current-balance",-1 * withdrawAmount)
+
+    document.querySelector('#withdraw-amount').value = "";
+})
+ 
+
+function inputValue(id){
+    let amount = document.querySelector(id).value;
+    let inputAmount = parseFloat(amount);
+    return inputAmount;
+}
+
+function updatedAmount(id,depositMoney){
+    let currentMoney = document.querySelector(id).innerText;
+    let currentNumberMoney = parseFloat(currentMoney);
+    let total = currentNumberMoney + depositMoney;
+    document.querySelector(id).innerText = total;
+}
